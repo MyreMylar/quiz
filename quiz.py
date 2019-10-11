ece0a63b7fc9015acd569d5789e5348a2647f2aa
Dan Lawrence <dan@myrespace.com>
2019-10-11 10:11:24 +0100

    Added week 2 challenges

--- quiz.py
+++ quiz.py
@@ -1,77 +1,146 @@
-# ---------------------------------------------------------------------------------------------------------------
-# Challenge 1
-# -------------
+# ------------------------------------------------------------------------------------------------
+# Overview of Quiz Week 2
+# --------------------------------------------------------------
+# We are going to recode the quiz program from last week to achieve the following
+# goals:
 #
-# Using some code from last week's quiz solution and some from
-# the pygame text example code file, open a pygame window and draw the
-# first question (with answers) in the loaded questions list
-# to the window.
+# Challenge 1:  Put some of the question and answer strings from last week
+#               into a newly defined QuizQuestion class, and then put those into
+#               a python list.
+# Challenge 2:  Use a single for loop over our new list to run the
+#               quiz logic (if...else) test, wait for input and keep score as
+#               to recreate the results of what we made last week.
+# Challenge 3:  Instead of writing the questions and answer strings in code,
+#               load them from a (provided) csv text file and store them in
+#               your list.
 #
-# HINTS
-# - Start by opening up the pygame_draw_text file in the code editor, and
-#   see if you can change the text
-# - You can reference the first item in a list using an index of 0
-#   like so: quiz_questions_list[0]
-# - You can treat an indexed list member like a variable of that type
-#   so you can access it's data members and functions,
-#   like so: quiz_questions_list[0].answer_a
-# ---------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------
+# But.. I wasn't here last week!/Didn't complete the code last week!
+# ------------------------------------------------------------------
+# In the 'quiz_last_week' file is some code that completes the challenges from last week.
+# You can use that to start from, or cut and paste your own code from last week here
+# if you have some.
+#
+# There are more comments below with extended hints for each of the three
+# challenges.
+# ------------------------------------------------------------------------------------------------
 
 
-# ---------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
+# Challenge 1 - part 1
+# ------------------------------------------------------------------------------------------------
+#
+#  Define a QuizQuestion class and create a few QuizQuestions.
+#
+#  In case you have forgotten how to create a class, here are a few tips:
+#
+# - We start defining a class with the 'class' keyword, a name, some brackets and a colon.
+#   After that you are ready to start defining class functions.
+# - Use the special '__init__' class function to pass in data and setup a class' data variables
+#   In our case it will be strings for the questions and the answers plus an extra variable
+#   for the correct answer's letter (A, B or C)
+# - When using class variables, and when calling class functions from within a class definition
+#   we have to use the self. keyword - see below for an example.
+# - We need to make a class that can accept five strings on creation,
+#   one for the question, three for the possible answers and
+#   one for the correct answer letter (A,B or C)
+# - I've created an example class below you can adapt into your QuizQuestion class, or just delete.
+#
+# --------------------------------------------------------------------------------------------------
+
+
+class ExampleClass:
+    def __init__(self, input_1, input_2, input_3, input_4, input_5):
+        self.class_variable_1 = input_1
+        self.class_variable_2 = input_2
+        self.class_variable_3 = input_3
+        self.class_variable_4 = input_4
+        self.class_variable_5 = input_5
+
+
+# creating a class instance
+my_example_class = ExampleClass("What is the best school?", "Rutlish", "Hogwarts", "School of Rock", "A")
+
+# accessing member data of a class and printing it
+print("a my_example_class variable:" + str(my_example_class.class_variable_1))
+
+
+# ------------------------------------------------------------------------------------------------
+# Challenge 1 - part 2
+# -----------------------
+#
+# Store a couple of QuizQuestions in a python list.
+#
+# TIPS
+#
+# - Create an empty python list variable using a pair of square brackets
+#   Like so: my_empty_list = []
+# - Add new items to the list using the append function.
+#   Like so: my_list.append(my_quiz_question_1)
+# ------------------------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------------------------
 # Challenge 2
-# ---------------------
+# ---------------
 #
-# Use pygame's keyboard input event system to change
-# the displayed question when the K_RETURN key is pressed.
+# Use a for loop to run through the QuizQuestions in our new list, print their questions
+# and answers, wait for user input and then check if the input letter was correct.
 #
-# HINTS
+#  TIPS
 #
-# - You may find it useful to have an integer variable representing
-#   the current index position in the list.
-# - You can index into a list using an integer variable just as with a
-#   a number, like so: quiz_questions_list[current_question_index]
-# ---------------------------------------------------------------------------------------------------------------
+# - This is basically recreating the logic of the program we created last week but
+#   using a loop to fill in the data rather than doing it all by hand question by question.
+# - Start by using the 'in' keyword to quickly run through all items in a list.
+#   Like so: for question in my_questions_list:
+# - Use the 'print' and 'input' function to put the data on the screen and wit for input.
+# - Compare the input with the correct answer variable of your quiz question class.
+# - track the score as before and output it when you finish the quiz
+# ------------------------------------------------------------------------------------------------
 
 
-# ---------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
 # Challenge 3
-# ----------------------
-# We are going to recreate the control flow of our text quiz program in pygame.
+# --------------
 #
-# First - Add a True or False bool variable to check whether the current question has been answered or not.
-# You will need to reset this variable to False each time we change to a new question.
+# Load QuizQuestions from a csv file and then store them in our list.
+#
+# - You will want to add this file loading code above the other bits of code you have written.
+# - There is a .csv file included in this weeks code folder
+#   folder. It contains a few questions.
+# - A csv file is just a text file with bits of data
+#   separated by new lines and commas. Thus the full name of the file
+#   format 'Comma Separated Values' - or the acronym 'csv' for short.
+# - The quiz questions in our CSV file are separated into rows that
+#   look like this:
+#       "Who designed the first computer?","Bill Gates","Steve Jobs","Charles Babbage",C
+#
+# - Python has support for loading csv files using the 'csv' library.
+#   you will need to use'import csv' to complete this challenge.
+# - To load data from a file you can use something like the following code -
+#
+#   with open('name_of_file.csv') as csv_file: # opens the file
+#       read_csv = csv.reader(csv_file, delimiter=',') # loads it as a CSV file
+#       for row in read_csv: # loops through each row in the file
+#           loaded_data_value_1 = row[0] # row indexes correspond to values in the file separated by commas
+#           loaded_data_value_2 = row[1]
+#
+# ------------------------------------------------------------------------------------------------
 
-# Second - Use pygame's keyboard events to check when the letters a, b or c are pressed and set
-# your current_question_answered variable to True.
-#
-# Third - Use an if statement to only allow the Return key to change to a new question if the
-# current_question_answered variable is True.
-#
-# Fourth - While current_question_answered is True display pygame text saying 'Correct' or 'False' depending on
-# if the correct answer was pressed in step 2.
-#
-# If you've done all that you should have the basics of a functioning pygame quiz.
-# ---------------------------------------------------------------------------------------------------------------
 
-
-# ---------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
 # Bonus Challenges
-# -----------------------------
-# Put some final polish on your quiz.
+# -------------------
 #
-# - Add back final score tracking and display and generally stop the quiz from crashing at the end.
-#   Perhaps add a 'retry quiz Y/N' prompt that starts the program over from the beginning.
-# - Use random.shuffle() to ask the questions in a random order.
-# - Add a 20 second time limit to answer each question and display the remaining time.
+# 1. See how far you can get recreating this quiz program in pygame
+#   - I have included a 'pygame_draw_text' file that shows how to
+#     create a pygame window, draw text and mess about with keyboard
+#     input.
 #
-# HINT
-# - You can track time with a clock = pygame.time.Clock().
-# - You can call clock.tick(60) to get the number of milliseconds since tick() was last called.
-# - By adding the value of tick to a variable each loop we can keep track of how many milliseconds have passed
-#   since we last set the variable to zero. You could also start a timer at 20,000 milliseconds (20 seconds)
-#   and subtract ticks from it.
-# - Don't forget to divide your counter by 1,000 before displaying it.
-# - You will also want to round and convert your number of seconds to remove all the decimal places.
-#   Try something like - str(int(round(question_timer/1000)))
-# ---------------------------------------------------------------------------------------------------------------
+# 2. Consider how to add:
+#    - a count down time in which a player must answer or fail the question.
+#    - multiple themed rounds of questions (e.g. music, film, sport)
+#    - Two (or more) players taking turns to answer questions and keeping scores
+#      for each of them
+#
+# ------------------------------------------------------------------------------------------------
--- quiz_last_week.py
+++ quiz_last_week.py
@@ -1,43 +1,50 @@
-import csv
-import random
+# this is one possible version of the code we worked on last week
 
+score = 0
 
-# Define class
-class QuizQuestion:
-    def __init__(self, question, answer_a, answer_b, answer_c, right_answer):
-        self.question = question
-        self.answer_a = answer_a
-        self.answer_b = answer_b
-        self.answer_c = answer_c
-        self.right_answer = right_answer
-        
+# Question 1
+print("What is the capital city of France?")
+print("A: Paris")
+print("B: London")
+print("C: Berlin")
 
-# load quiz questions from file to list
-quiz_questions_list = []
-with open("questions.csv") as questions_file:
-    read_csv = csv.reader(questions_file, delimiter=',')
-    for row in read_csv:
-        if len(row) == 5:
-            quiz_questions_list.append(QuizQuestion(row[0], row[1], row[2], row[3], row[4]))
+answer = input()
+answer = answer.lower()
 
+if answer == 'a':
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong!")
 
-# run quiz with a for loop
-random.shuffle(quiz_questions_list)
-total_score = 0
-for quiz_question in quiz_questions_list:
-    print(quiz_question.question)
-    print("A: " + quiz_question.answer_a)
-    print("B: " + quiz_question.answer_b)
-    print("C: " + quiz_question.answer_c + "\n")
+# Question 2
+print("Who is the prime minister of the UK?")
+print("A: Donald Trump")
+print("B: David Cameron")
+print("C: Teresa May")
 
-    playerInput = input("Type answer letter and then press enter: ")
+answer = input()
+answer = answer.lower()
 
-    if playerInput.upper() == quiz_question.right_answer:
-        print("\nCorrect!\n\n")
-        total_score += 1
-    else:
-        print("\nWrong.\n\n")
+if answer == 'c':
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong!")
 
-# print final results
-print("End of Quiz")
-print("Total score = " + str(total_score) + "/" + str(len(quiz_questions_list)))
+# Question 3
+print("When was the battle of Hastings fought?")
+print("A: 1066")
+print("B: 1776")
+print("C: 1966")
+
+answer = input()
+answer = answer.lower()
+
+if answer == 'a':
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong!")
+
+print("Final Score: " + str(score) + "/3")
